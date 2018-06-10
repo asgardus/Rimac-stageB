41ff037d89c7b4d4b6155fa1488300a2bdf1dc36
asgardus <gandhiansh95@gmail.com>
2018-06-10 13:07:09 +0200

    junk

--- Trials/phase-portrait/phasepotrait_trial.m
+++ Trials/phase-portrait/phasepotrait_trial.m
@@ -2,8 +2,8 @@ clc;
 clear all;
 
 f = @(t,Y) [Y(2); -sin(Y(1))];
-r = linspace(-60,60,60).*pi./180;
-beta = linspace(-40,40,60).*pi./180;
+r = linspace(-60,60,20).*pi./180;
+beta = linspace(-40,40,20).*pi./180;
 
 % creates two matrices one for all the x-values on the grid, and one for
 % all the y-values on the grid. Note that x and y are matrices of the same
@@ -12,7 +12,7 @@ beta = linspace(-40,40,60).*pi./180;
 size(x)
 size(y)
 %% variables
-delta=20*pi/180;
+delta=30*pi/180;
 m=1725;
 Jz=1300;
 a=1.35;
@@ -30,26 +30,28 @@ af=atan(x+((a.*y)./vx))-delta;
 ar=atan(x-((b.*y)./vx));
 fz=fzf;
 l=.1;
-for i=1:60
-    for k=1:60
+for i=1:20
+    for k=1:20
         a=af(i,k);
 sim('MED2018_TireTest_Example.slx')
 ff(i,k)=Fy(end);
     end
 end
 fz=fzr;
-for i=1:60
-    for k=1:60
+for i=1:20
+    for k=1:20
         a=ar(i,k);
 sim('MED2018_TireTest_Example.slx')
 fr(i,k)=Fy(end);
     end
-end
-
+ end
+% ay=(ff+fr)./m;
+% mz=(a.*ff-b.*fr)./(m*(a+b));
+% plot(ay,mz)
 x1dot=(1./Jz).*(a.*ff-b.*fr);
 x2dot=((ff+fr)-r)./(m*vx);
 quiver(x.*180/pi,y.*180/pi,x2dot.*180/pi,x1dot.*180/pi); figure(gcf)
 xlabel('\beta')
 ylabel('yaw')
 hold off
-find(x1dot==0)
+% find(x1dot==0)
