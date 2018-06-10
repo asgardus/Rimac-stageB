2351db63a51f9b33622151eaaf6a652287c0ab76
asgardus <gandhiansh95@gmail.com>
2018-06-10 22:11:31 +0200

    updated phase portrait code

--- Trials/phase-portrait/phasepotrait_trial.m
+++ Trials/phase-portrait/phasepotrait_trial.m
@@ -1,33 +1,28 @@
-clc;
-clear all;
+clc
+clear all
 
-f = @(t,Y) [Y(2); -sin(Y(1))];
 r = linspace(-60,60,20).*pi./180;
-beta = linspace(-40,40,20).*pi./180;
+vy = linspace(-40,40,20).*pi./180;
 
 % creates two matrices one for all the x-values on the grid, and one for
 % all the y-values on the grid. Note that x and y are matrices of the same
 % size and shape, in this case 20 rows and 20 columns
-[x,y] = meshgrid(beta,r);
-size(x)
-size(y)
+[x,y] = meshgrid(vy,r);
 %% variables
-delta=30*pi/180;
+delta=0*pi/180;
 m=1725;
 Jz=1300;
 a=1.35;
 b=1.15;
-Cf=75;
-Cr=135;
-vx=30;
+vx=20;
 g=9.81;
 fzf=m*g*b/(a+b);
 fzr=m*g*a/(a+b);
 %% computation
 x2 = zeros(size(x));%beta
 x1 = zeros(size(y));%r
-af=atan(x+((a.*y)./vx))-delta;
-ar=atan(x-((b.*y)./vx));
+af=atan(((x+a*y)/vx)-delta);
+ar=atan((x-b*y)/vx);
 fz=fzf;
 l=.1;
 for i=1:20
@@ -48,10 +43,13 @@ fr(i,k)=Fy(end);
 % ay=(ff+fr)./m;
 % mz=(a.*ff-b.*fr)./(m*(a+b));
 % plot(ay,mz)
-x1dot=(1./Jz).*(a.*ff-b.*fr);
-x2dot=((ff+fr)-r)./(m*vx);
-quiver(x.*180/pi,y.*180/pi,x2dot.*180/pi,x1dot.*180/pi); figure(gcf)
-xlabel('\beta')
+x1dot=((ff*cos(delta)+fr)-r)./(m*vx);
+x2dot=(1/Jz)*(a*ff*cos(delta)-b*fr);
+
+%% plotting
+figure(1);
+quiver(x,y,x1dot,x2dot); figure(gcf)
+xlabel('v_y')
 ylabel('yaw')
 hold off
 % find(x1dot==0)
